1316ba24c55de5d5ee13a222da27d8e989031d2b
Muhammad Ahmed <93033015+jerryj12@users.noreply.github.com>
2023-10-03 17:53:57 +0500

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "This is a sample script executed as part of a GitHub Actions workflow."
-echo "You can customize this script to perform your desired actions."
+# Set your Gmail email credentials
+SMTP_SERVER="smtp.gmail.com"
+SMTP_PORT="587"
+SMTP_USER="ahmed.sociocast@gmail.com"
+SMTP_PASSWORD="irmw zrmx xjiv fovf"
+RECIPIENT="ahmed.sociocast@gmail.com"  # Replace with the recipient's email address
+SUBJECT="Test Email"
+MESSAGE="This is a test email sent from a shell script."
+
+# Use the 'mail' command to send the email
+echo "$MESSAGE" | mail -s "$SUBJECT" -S smtp="$SMTP_SERVER":"$SMTP_PORT" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="$SMTP_USER" -S smtp-auth-password="$SMTP_PASSWORD" "$RECIPIENT"
